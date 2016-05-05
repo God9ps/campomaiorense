9912f9cf8f52355b97dcb4692d4459924e27b6f6
God9ps <pauloamserrano@gmail.com>
2016-05-05 21:46:02 +0100

    Added files via upload

---
+++ about.php
@@ -0,0 +1,95 @@
+<?php
+ include "header.html";
+ ?>
+<!-- about -->
+<div class="about">
+	 <div class="container">
+		 <ol class="breadcrumb">
+		  <li><a href="index.html">Home</a></li>
+		  <li class="active">A nossa história</li>
+		 </ol>
+		 <h2 style="color:green">A NOSSA HISTÓRIA</h2>
+		 <div class="about-head text-justify">
+			 <p>O Sporting Clube Campomaiorense nasceu a 1 de Julho de 1926, sendo o seu primeiro presidente Domingos António Rabiais. É desconhecido quando se tornou a 27ª Filial do Sporting Clube de Portugal, mas pela data de fundação sua e das filiais com nº próximo, é provável que tenha sido quase de imediato. Filiou-se na Associação de Futebol de Portalegre logo em 1926.
+				 O SC Campomaiorense só começou a participar em provas nacionais na época de 1945/46, em que disputou a 2ª divisão nacional O Clube utiliza o Estádio Capitão César Correia, que é propriedade da Casa do Povo de Campo Maior. Na maioria das temporadas, o Clube jogou na 2ª e 3ª divisão, com algumas passagens pelas divisões distritais, tendo vencido a série D da 3ª Divisão Nacional em 1960/61.
+
+				 No final dos anso 80 o clube foi apoiado pela família Nabeiro, ganhando o Campeonato Nacional da 2ª Divisão B em 1992/93, e ascendendo à 1ª divisão em 1995/96, e conseguindo, até abandonar o futebol profissional, 5 presenças na 1ª divisão, e ainda vencer a 2ª Divisão em 1996/97.
+
+
+				 Campomaiorense na década de 20
+				 Com a mudança do símbolo do clube proposta em 1997 e aceite em assembleia geral extraordinária no dia 2 de Maio de 1998, sendo a partir dessa data um galgo, a direcção do clube lidarada por João Manuel Nabeiro propunha-se reunir em torno desta ideia, toda uma região alentejana que, em termos desportivos, tinha o Campomaiorense como expoente máximo, com o lema “Campomaiorense, mais que um clube, uma região”.
+
+				 Para além do novo símbolo, nos Estatutos do Clube ficou também consagrada a possibilidade de utilizar as cores amarelo torrado e grená, para além das tradicionais verde e branca. Hoje em dia, o Campomaiorense, é conhecido como o Clube do Galgo, apesar de ser ainda uma Filial do Sporting Clube de Portugal.
+
+				 Campomaiorense em 1935
+				 No final da temporada 2001/02, com o declínio desportivo verificado, o SC Campomaiorense extinguiu a equipa de futebol profissional. Manteve as camadas jovens, e a partir de 2006/07 voltou a ter uma equipa sénior amadora a competir nas divisões distritais de Portalegre.
+</p>
+			</div>
+		 <div class="container-fluid">
+			 <div class="about-pic img-responsive">
+				 <img style="border-radius: 20px" src="images/site/isaias.jpeg" alt=""/>
+			 </div>
+		 </div>
+			 <div class="clearfix"></div>
+		 </div>
+		 <div class="abt_text">
+				<div class="col-md-4 values">
+					 <h3 style="color: green">Os nossos valores</h3>
+					 <div class="value-grd">
+						 <span>01.</span>
+						 <h4>Mais que um clube, uma paixão!</h4>
+
+					 </div>
+					 
+				</div>
+				<div class="col-md-4 skills social">
+					 <h3 style="color: green">Os nossos idolos</h3>
+					 <h4>Por Campo Maior passaram grandes nomes do futebol, não só nacional como também internacional</h4>
+					 <p>A lista é longa, e deixamos muitos de fora que marcaram a sua época enquanto jogadores do Campomaiorense, mas vamos listar alguns dos idolos que passaram pelo nosso clube.</p>
+
+					<ul>
+						<li>
+							<span><img src="images/site/jimmy.jpg" class="img-circle" style="width: 80px"></span>
+							<a href="https://pt.wikipedia.org/wiki/Jimmy_Floyd_Hasselbaink">Jimmy Floyd Hasselbaink</a>
+							<a href="https://twitter.com/jf9hasselbaink?lang=pt"><img src="images/site/twitter-icon.png" style="width: 30px;"></a>
+							<a href="https://www.youtube.com/watch?v=u3F_Bpqpg7Y"><img src="images/site/youtube-icon.png" style="width: 30px;"></a>
+						</li>
+
+						<li>
+							<span><img src="images/site/isaias1.jpg" class="img-circle" style="width: 80px"></span>
+							<a href="http://www.zerozero.pt/jogador.php?id=14736&epoca_id=121">Isaías</a>
+							<a href="https://www.youtube.com/watch?v=dc_itbknrOM"><img src="images/site/youtube-icon.png" style="width: 30px;"></a>
+
+						</li>
+
+						<li>
+							<span><img src="images/site/beto.gif" class="img-circle" style="width: 80px"></span>
+							<a href="https://pt.wikipedia.org/wiki/Jimmy_Floyd_Hasselbaink">Roberto Severo (Beto)</a>
+							<a href="https://www.facebook.com/beto.severo"><img src="images/site/facebook-icon.png" style="width: 30px;"></a>
+							<a href="https://www.youtube.com/watch?v=qK0kdx1siEE"><img src="images/site/youtube-icon.png" style="width: 30px;"></a>
+						</li>
+						<li>
+							<span><img src="images/site/rolando.jpg" class="img-circle" style="width: 80px"></span>
+							<a href="https://pt.wikipedia.org/wiki/Jimmy_Floyd_Hasselbaink">Rolando</a>
+							<a href="https://www.facebook.com/pages/Rolando-Jorge-Pires-da-Fonseca/108609879163102"><img src="images/site/facebook-icon.png" style="width: 30px;"></a>
+							<a href="https://www.youtube.com/watch?v=TRC0i0gp5vU"><img src="images/site/youtube-icon.png" style="width: 30px;"></a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-4 testi">
+					 <h3 style="color: green">Testemunhos</h3>
+				     <p>Devo ao Campomaiorense a minha ida para Portugal e agradeço a oportunidade de jogar por essa equipa.</p>
+					 <a href="https://www.youtube.com/watch?time_continue=217&v=cy-PbPDsGPs" TARGET="_blank">LAELSON LIMA</a>
+					 <p>Sou um filho de Campo Maior. Estarei sempre disponível para ajudar o clube.</p>
+					 <a href="http://www.maisfutebol.iol.pt/geral/19-06-2002/campomaiorense-joao-manuel-nabeiro-lembra-os-12-anos-que-deu-ao-clube">JOÃO MANUEL NABEIRO</a>
+					 <p>Campo Maior, para mim das coisas mais importantes na minha vida desportiva.</p>
+					 <a href="http://joaquimfolgadonoticias.blogspot.pt/2016/03/manuel-fernandes-primeiro-tecnico-que.html">MANUEL FERNANDES</a>
+				</div>
+				<div class="clearfix"></div>
+		 </div>		
+	 </div>
+</div>
+<!-- //about -->
+<?php
+include "footer.html";
+?>
---
+++ index.php
@@ -0,0 +1,208 @@
+
+<?php
+include "header.html";
+?>
+
+<!---start-content----->
+<div class="banner-bottom-grids">
+<!--    <div class="container">-->
+<!--        <div class="col-md-6 banner-text-info clr1">-->
+<!--            <i class="icon1"></i>-->
+<!--            <div class="bnr-text">-->
+<!--                <h3>CHAMPIONS LEAGUE</h3>-->
+<!--                <p>Vestibulum malesuada nisi sit amet justo ullamcorper, non convallis justo consequat Integer et urna bibendum elit accumsan interdum.</p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="col-md-6 banner-text-info clr2">-->
+<!--            <i class="icon2"></i>-->
+<!--            <div class="bnr-text">-->
+<!--                <h3>NEWS & EVENTS</h3>-->
+<!--                <p>Vestibulum malesuada nisi sit amet justo ullamcorper, non convallis justo consequat Integer et urna bibendum elit accumsan interdum.</p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="clearfix"></div>-->
+<!---->
+<!--        <div class="col-md-6 banner-text-info clr3 btm">-->
+<!--            <i class="icon3"></i>-->
+<!--            <div class="bnr-text">-->
+<!--                <h3>TRAINING</h3>-->
+<!--                <p>Vestibulum malesuada nisi sit amet justo ullamcorper, non convallis justo consequat Integer et urna bibendum elit accumsan interdum.</p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="col-md-6 banner-text-info clr4 btm">-->
+<!--            <i class="icon4"></i>-->
+<!--            <div class="bnr-text">-->
+<!--                <h3>FINAL TOURNAMENT</h3>-->
+<!--                <p>Vestibulum malesuada nisi sit amet justo ullamcorper, non convallis justo consequat Integer et urna bibendum elit accumsan interdum.</p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="clearfix"></div>-->
+<!--    </div>-->
+<!--</div>-->
+</div>
+<!-- //banner -->
+<div class="container text-center popular">
+    <h3 style="color: green">O CAMPOMAIORENSE SOMOS NÓS</h3>
+    <div class="carousel slide row" data-ride="carousel" data-type="multi" data-interval="2000" id="fruitscarousel">
+
+        <div class="carousel-inner">
+            <div class="item active">
+                <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="images/c2.jpg" width="150px" class="img-responsive"></a></div>
+            </div>
+            <div class="item">
+                <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="images/c2.jpg" width="150px" class="img-responsive"></a></div>
+            </div>
+            <div class="item">
+                <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="images/c2.jpg" width="150px" class="img-responsive"></a></div>
+            </div>
+            <div class="item">
+                <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="images/c2.jpg" width="150px" class="img-responsive"></a></div>
+            </div>
+            <div class="item">
+                <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="images/c2.jpg" width="150px" class="img-responsive"></a></div>
+            </div>
+            <div class="item">
+                <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="images/c2.jpg" width="150px" class="img-responsive"></a></div>
+            </div>
+        </div>
+
+        <a class="left carousel-control" href="#fruitscarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
+        <a class="right carousel-control" href="#fruitscarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+
+    </div>
+</div>
+
+<div class="banner-bottom-grids"></div>
+<!-- content -->
+<div class="content">
+    <div class="container">
+        <div class="content-grids text-center popular">
+            <h3 style="color: green">COMPETIÇÕES</h3>
+            <div class="col-md-4 content-grid1">
+                <img src="images/c1.jpg" alt=""/>
+                <h3 style="color: green">Champion's League</h3>
+                <p>Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+                    Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker.</p>
+                <a href="#">Read More..</a>
+            </div>
+            <div class="col-md-4 content-grid1">
+                <img src="images/c2.jpg" alt=""/>
+                <h3 style="color: green">Women's Cup</h3>
+                <p>Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+                    Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker.</p>
+                <a href="#">Read More..</a>
+            </div>
+            <div class="col-md-4 content-grid1">
+                <img src="images/c3.jpg" alt=""/>
+                <h3 style="color: green">Final Tournment</h3>
+                <p>Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+                    Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker.</p>
+                <a href="#">Read More..</a>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</div>
+<!--- //content--->
+<!-- content-bottom -->
+<div class="content-info">
+    <div class="container">
+        <div class="content-bottom-grids">
+            <div class="col-md-4 popular">
+                <div class="panel">
+                    <h3 style="color: green">NOTICIAS</h3>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <ul class="demo1">
+                                    <li class="news-item">
+                                        <table cellpadding="4">
+                                            <tr>
+                                                <td><img src="images/1.png" width="60" class="img-rounded" /></td>
+                                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></td>
+                                            </tr>
+                                        </table>
+                                    </li>
+                                    <li class="news-item">
+                                        <table cellpadding="4">
+                                            <tr>
+                                                <td><img src="images/2.png" width="60" class="img-rounded" /></td>
+                                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></td>
+                                            </tr>
+                                        </table>
+                                    </li>
+                                    <li class="news-item">
+                                        <table cellpadding="4">
+                                            <tr>
+                                                <td><img src="images/3.png" width="60" class="img-rounded" /></td>
+                                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></td>
+                                            </tr>
+                                        </table>
+                                    </li>
+                                    <li class="news-item">
+                                        <table cellpadding="4">
+                                            <tr>
+                                                <td><img src="images/4.png" width="60" class="img-rounded" /></td>
+                                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></td>
+                                            </tr>
+                                        </table>
+                                    </li>
+                                    <li class="news-item">
+                                        <table cellpadding="4">
+                                            <tr>
+                                                <td><img src="images/5.png" width="60" class="img-rounded" /></td>
+                                                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></td>
+                                            </tr>
+                                        </table>
+                                    </li>
+
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 welcome-pic">
+                <h3 style="color: green">AVISOS</h3>
+                <h4>Morbi sed arcu mollis, elementum erat venenatis, tincidunt tellus.</h4>
+                <img src="images/cnt.ab.jpg" alt=""/>
+                <p>Aenean ut condimentum magna, mattis pretium massa. Sed sollicitudin ullamcorper auctor. Duis vestibulum velit id augue pulvinar egestas.
+                    Morbi sed orci auctor, feugiat felis at, fermentum magna. In ac egestas lectus.</p>
+            </div>
+            <div class="col-md-4 coach">
+                <h3 style="color: green">JOGADORES EM DESTAQUE</h3>
+                <div class="coch-grid chr">
+                    <div class="coach-pic">
+                        <img src="images/ch1.jpg" alt=""/>
+                    </div>
+                    <div class="coach-pic-info">
+                        <h4><a href="#">Phasellus at Tellus</a></h4>
+                        <h5>Aenean vestibulum </h5>
+                        <p>Donec ornare massa at velit fringilla, condimentum magna ornare tincidunt nulla dignissim.</p>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="coch-grid">
+                    <div class="coach-pic">
+                        <img src="images/ch2.jpg" alt=""/>
+                    </div>
+                    <div class="coach-pic-info">
+                        <h4><a href="#">Phasellus at Tellus</a></h4>
+                        <h5>Aenean vestibulum </h5>
+                        <p>Donec ornare massa at velit fringilla, condimentum magna ornare tincidunt nulla dignissim.</p>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</div>
+<!-- //content-bottom -->
+<?php
+include "footer.html";
+?>
